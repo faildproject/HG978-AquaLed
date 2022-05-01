0b2ca491c76dcd87b17d493f04888607218d66fb
Philipp Heise <philipp.heise.2012@gmail.com>
2022-05-01 21:33:52 +0200

    ?

--- src/main.cpp
+++ src/main.cpp
@@ -12,25 +12,76 @@ const int Red_Ch = 0;
 const int Green_Ch = 1;
 const int Blue_Ch = 2;
 const int White_Ch = 3;
-const int resolution = 11;
+const int resolution_bit = 11;
+const int resolution_steps = 2048;
+
+float MorgenSonne = 1000;
+float MittagsSonne = 6000;
+float AbendSonne =1000;
+
+float Mondlicht = 4000;
 
 void writeRGB(float red, float green, float blue){
-  uint32_t red_pwm = red * 2048.;
-  uint32_t green_pwm = green * 2048.;
-  uint32_t blue_pwm = blue * 2048.;
-  Serial.printf("R%d,G%d,B%d \r\n",red_pwm,green_pwm,blue_pwm);
+  uint32_t red_pwm = red * float(resolution_steps);
+  uint32_t green_pwm = green * float(resolution_steps);
+  uint32_t blue_pwm = blue * float(resolution_steps);
+  //Serial.printf("R%d,G%d,B%d \r\n",red_pwm,green_pwm,blue_pwm);
   ledcWrite(Red_Ch, red_pwm);
   ledcWrite(Green_Ch, green_pwm);
   ledcWrite(Blue_Ch, blue_pwm);
 }
 
+void sunrise(uint32_t time_in_s, uint32_t steps){
+  uint32_t _t_last = millis();
+  uint32_t _period = (time_in_s*1000)/steps;
+  uint32_t _steps_red = steps;
+  uint32_t _steps_green = float(_steps_red) * 0.4;
+  uint32_t _steps_blue = float(_steps_green) * 0.7;
+  uint32_t _steps_white = float(_steps_blue) * 0.6;
+  float red = 0.0;
+  float green = 0.0;
+  float blue = 0.0;
+  float white = 0.0;
+  
+  while(millis()<(time_in_s*1000)){
+    if((millis()-_t_last)>=_period){
+      _t_last=millis();
+
+      // Goto 100%
+      red += 1.0/ float(_steps_red);
+      
+      if (red >= 0.3)
+      {
+        green += 1.0/ float(_steps_green);
+      }
+
+      if (green >= 0.6){
+        blue += 1.0/ float(_steps_blue);
+      }
+
+      if (blue >= 0.5){
+        white += 1.0/ float(_steps_white);
+      }
+
+      ledcWrite(White_Ch, white);
+      writeRGB(red,green,blue);
+      Serial.printf("t: %d Step: %d \t R%.2f,G%.2f,B%.2f,W%.2f \r\n",millis(),steps,red,green,blue,white);
+      steps--;
+      if(steps<=0){
+        return;
+      }
+    }
+  }
+
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  ledcSetup(Red_Ch, freq, resolution);
-  ledcSetup(Green_Ch, freq, resolution);
-  ledcSetup(Blue_Ch, freq, resolution);
-  ledcSetup(White_Ch, freq, resolution);
+  ledcSetup(Red_Ch, freq, resolution_bit);
+  ledcSetup(Green_Ch, freq, resolution_bit);
+  ledcSetup(Blue_Ch, freq, resolution_bit);
+  ledcSetup(White_Ch, freq, resolution_bit);
 
   ledcAttachPin(RED_PIN,Red_Ch);
   ledcAttachPin(GREEN_PIN,Green_Ch);
@@ -42,41 +93,28 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (uint32_t i = 1000; i <= 12000; i++)
-  {
-    /* code */
-    KRGB.convert_NB(i,100);
+    sunrise(15,2048);
     
-    delay(2);
-    Serial.printf("K,%d->",i);
+    /*
+    KRGB.convert_NB(MorgenSonne,100);
+    Serial.printf("Sonnenaufgang ");
     writeRGB(KRGB.red(),KRGB.green(),KRGB.blue());
-  }
-
-  for (uint32_t i = 12000; i >= 1000; i--)
-  {
-    /* code */
-    KRGB.convert_NB(i,100);
-    Serial.printf("K,%d->",i);
-    delay(2);
+    delay(10000);
+  
+    KRGB.convert_NB(MittagsSonne,100);
+    Serial.printf("Mittagssonne ");
     writeRGB(KRGB.red(),KRGB.green(),KRGB.blue());
-  }
-  // increase the LED brightness
-  for(int32_t dutyCycle = 0; dutyCycle <= 2048; dutyCycle++){   
-    // changing the LED brightness with PWM
-    Serial.printf("W%d\r\n",dutyCycle);
-    ledcWrite(White_Ch, dutyCycle);
-    delay(5);
-  }
+    delay(10000);
 
-  // decrease the LED brightness
-  for(int32_t dutyCycle = 2048; dutyCycle >= 0; dutyCycle--){
-    // changing the LED brightness with PWM
-    Serial.printf("W%d\r\n",dutyCycle);
-    ledcWrite(White_Ch, dutyCycle);   
-    delay(5);
-  }
-  
-  
-  
+    KRGB.convert_NB(AbendSonne,100);
+    Serial.printf("Mittagssonne ");
+    writeRGB(KRGB.red(),KRGB.green(),KRGB.blue());
+    delay(10000);
+
+    KRGB.convert_NB(Mondlicht,100);
+    Serial.printf("Mondlicht ");
+    writeRGB(KRGB.red(),KRGB.green(),KRGB.blue());
+    delay(10000);
+    */
+
 }
